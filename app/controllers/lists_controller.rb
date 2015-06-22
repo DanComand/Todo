87ed2c706f2d813a27605535269e4d271736b8c0
Dan Comand <dcomand@gmail.com>
2015-06-22 18:56:35 -0400

    creating list model, views, and form partials.

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+  def index
+  	@lists = List.all
+  end
+
+  def new
+  	@list = List.new
+  end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+  def edit
+  	@list = List.new(product_params)
+
+  	if @list.save
+  		redirect_to lists_url
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  	@list = List.find(params[:id])
+
+  	if @list.update_attributes(product_params)
+  		redirect_to list_path(@list)
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@list = List.find(params[:id])
+  	@list.destroy
+  	redirect_to lists_path
+  end
+
+  private
+  def list_params
+  	params.require(:list).permit(:name, :user_id)
+  end
+end
